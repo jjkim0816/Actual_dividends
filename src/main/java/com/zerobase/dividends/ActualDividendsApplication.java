47fe1f88dfce95d7ab2ec5acf1e8097920ac18c3
jjkim0816 <jjkim0816@gmail.com>
2023-10-29 22:17:39 +0900

    first commit

---
+++ src/main/java/com/zerobase/dividends/ActualDividendsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.dividends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualDividendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualDividendsApplication.class, args);
+	}
+
+}
